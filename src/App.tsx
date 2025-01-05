42035ef54f7aa1a639e84963cd044ec123d01f8b
Cida Rosa <lucita.rosa@gmail.com>
2025-01-05 15:01:46 -0300

    Implementado contador crescente e decrescente

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { useState } from "react"
 
 function App() {
 
+  // const count = 0;
+  // desestruturação
+  // count => dado
+  // setCount => função que altera o dado
+  // useState(0) -> valor inicial da variável, senão começa com undefined
+  const [count, setCount] = useState(0);
+
+  //comportamento do botão incrementar
+  function handleClick() {
+    setCount(count + 1);
+  }
+  function handleClickDec() {
+    setCount(count - 1);
+  }
+
   return (
-    <h1>Olá Mundo</h1>
+    <div>
+       <h1>Contagem = {count} </h1>
+       <button onClick={handleClick}>Incrementar</button>
+       <button onClick={handleClickDec}>Decrementar</button>
+    </div>
+   
   )
 }
 
